5a96b8bdf0c819d3358beb8739187cd8bdf6e1e5
Admin <Admin@Admin>
2019-10-20 00:11:49 +0700

    Create method abstract class Phone

--- OOP/src/OOP/Phone.java
+++ OOP/src/OOP/Phone.java
@@ -1,4 +1,9 @@
 package OOP;
-public abstract class Phone {
 
+public abstract class Phone {
+	abstract void insertPhone(String name);
+	abstract void removePhone(String name);
+	abstract void updatePhone(String name, String newphone);
+	abstract void searchPhone(String name);
+	abstract void sort();
 }
