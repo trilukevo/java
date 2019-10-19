fe8598fea2bb9115ab0a765f490345028c922705
Admin <Admin@Admin>
2019-10-20 00:19:01 +0700

    Add Class PhoneBook extends Phone

--- OOP/src/OOP/Phone.java
+++ OOP/src/OOP/Phone.java
@@ -1,7 +1,7 @@
 package OOP;
 
 public abstract class Phone {
-	abstract void insertPhone(String name);
+	abstract void insertPhone(String name, String phone);
 	abstract void removePhone(String name);
 	abstract void updatePhone(String name, String newphone);
 	abstract void searchPhone(String name);
---
+++ OOP/src/OOP/PhoneBook.java
@@ -0,0 +1,37 @@
+package OOP;
+
+import java.util.ArrayList;
+
+public class PhoneBook extends Phone {
+	ArrayList<Phone> phoneList = new ArrayList<Phone>();
+	
+
+	@Override
+	void insertPhone(String name, String phone) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void removePhone(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void updatePhone(String name, String newphone) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void searchPhone(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void sort() {
+		// TODO Auto-generated method stub
+		
+	}}
